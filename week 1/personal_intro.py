e3d72c5a36448b6bf7a9b71258ac804e09aae187
sanjay1011777 <leetsan2005@gmail.com>
2025-12-31 09:24:14 +0530

    Add files via upload

---
+++ week 1/personal_intro.py
@@ -0,0 +1,9 @@
+
+# Asking user for details
+name = input("What is your name? ")
+age = input("How old are you? ")
+hobby = input("What is your favorite hobby? ")
+
+# Displaying personalized welcome message
+print("\n🎉 Welcome", name + "! 🎉")
+print(f"You are {age} years old and you love {hobby}.")
