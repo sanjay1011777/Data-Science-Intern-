6e1becbd981936dd3128eac23af252b6779116f3
sanjay1011777 <leetsan2005@gmail.com>
2025-12-31 09:21:07 +0530

    Delete personal_intro.py

--- personal_intro.py
+++
@@ -1,9 +0,0 @@
-
-# Asking user for details
-name = input("What is your name? ")
-age = input("How old are you? ")
-hobby = input("What is your favorite hobby? ")
-
-# Displaying personalized welcome message
-print("\n🎉 Welcome", name + "! 🎉")
-print(f"You are {age} years old and you love {hobby}.")
